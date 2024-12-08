c955328601c92edc77cd5987c849725ab1538142
Philipp Adaemmer <adaemmerp@uni-greifswald.de>
2024-12-08 22:03:31 +0100

    modify  logo

--- docs/logo.jl
+++ docs/logo.jl
@@ -7,13 +7,13 @@ Drawing(600, 600, "op_logo.svg")
 p_size = 60
 
 origin()
-setcolor(1, 1, 1)
+setcolor(1, 1, .9)
 squircle(O, 300, 220, rt=0.5, action = :fill)
 
 # Three points 
-point1 = Point(-190, -110)
-point2 = Point(-10, 130)
-point3 = Point(210, -20)
+point1 = Point(-220, 10)
+point2 = Point(10, 150)
+point3 = Point(210, -100)
 
 # First point
 setcolor(Luxor.julia_green)
@@ -54,27 +54,4 @@ strokepath()
 
 finish()
 preview()
-end
-
-logo = begin
-    Drawing(200, 200, "logo.svg")
-    origin()
-    scale(3.4)
-
-    fontface("Arial Bold")
-    fontsize(50)
-
-    # Define center points and corners of triangle
-    center = Point(0, 4)
-    corners = ngon(center, 16, 3, -0.75π / 2; vertices=true)
-
-    # Draw three blended partials
-    for (i, c) in enumerate(corners)
-        b = blend(c, 0, center, 50, colors[i], "black")
-        setblend(b)
-        text("∂", c; valign=:middle, halign=:center, angle=0.68π + 2π / 3 * i)
-    end
-
-    finish()
-    preview()
 end
