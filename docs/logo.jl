3f99e59bfd882e04d7983a2aa81d171efaf8249b
Philipp Adaemmer <adaemmerp@uni-greifswald.de>
2024-12-08 21:45:31 +0100

    modify  logo

--- docs/logo.jl
+++ docs/logo.jl
@@ -10,11 +10,6 @@ origin()
 setcolor(0.2, 0.2, 0.3)
 squircle(O, 300, 220, rt=0.5, action = :fill)
 
-# Write package name
-#fontsize(68)
-#setcolor(Luxor.julia_purple)  
-#text("OrdinalPatterns.jl", Point(0, 220), halign=:center, valign=:bottom)
-
 # Three points 
 point1 = Point(-190, -110)
 point2 = Point(-10, 130)
@@ -38,13 +33,47 @@ circle(point3, p_size, action = :fill)
 setcolor(Luxor.julia_blue)
 circle(point3, p_size, action = :stroke)
 
-# Draw lines to connect the points
-setcolor(Luxor.julia_blue)
+# Draw first lines
 setline(8)
-line(point1, point2, action = :stroke)
-line(point2, point3, action = :stroke)
+green_red = blend(point1, point2, Luxor.julia_green, Luxor.julia_red)
+setblend(green_red)
+line(point1, point2)
+strokepath()
+
+# Draw second line
+red_purple = blend(point2, point3, Luxor.julia_red, Luxor.julia_purple)
+setblend(red_purple)
+line(point2, point3)
+strokepath()
+
+# setcolor(Luxor.julia_blue)
+# setline(8)
+# line(point1, point2, action = :stroke)
+# line(point2, point3, action = :stroke)
 
 finish()
 preview()
 end
 
+logo = begin
+    Drawing(200, 200, "logo.svg")
+    origin()
+    scale(3.4)
+
+    fontface("Arial Bold")
+    fontsize(50)
+
+    # Define center points and corners of triangle
+    center = Point(0, 4)
+    corners = ngon(center, 16, 3, -0.75π / 2; vertices=true)
+
+    # Draw three blended partials
+    for (i, c) in enumerate(corners)
+        b = blend(c, 0, center, 50, colors[i], "black")
+        setblend(b)
+        text("∂", c; valign=:middle, halign=:center, angle=0.68π + 2π / 3 * i)
+    end
+
+    finish()
+    preview()
+end
