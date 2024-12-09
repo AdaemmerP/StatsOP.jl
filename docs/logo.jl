8d9199faaff7eefdd3b4944fcb8ffa6c5318b604
Philipp Adaemmer <adaemmerp@uni-greifswald.de>
2024-12-09 12:01:46 +0100

    modify  logo

--- docs/logo.jl
+++ docs/logo.jl
@@ -18,14 +18,14 @@ using Colors
 
     # First point
     setline(30) # Line width for :stroke
-    setcolor(Luxor.julia_green)
-    circle(point1, p_size, action=:fill)
+    #setcolor(Luxor.julia_green)
+    #circle(point1, p_size, action=:fill)
     setcolor(Luxor.julia_blue)
     circle(point1, p_size, action=:stroke)
 
     # Second point
-    setcolor(Luxor.julia_red)
-    circle(point2, p_size, action=:fill)
+    #setcolor(Luxor.julia_red)
+    #circle(point2, p_size, action=:fill)
     setcolor(Luxor.julia_blue)
     circle(point2, p_size, action=:stroke)
 
@@ -34,6 +34,20 @@ using Colors
     circle(point3, p_size, action=:fill)
     setcolor(Luxor.julia_blue)
     circle(point3, p_size, action=:stroke)
+
+    # Blend to fill
+    green_red = blend(Point(0, 0), 5, Point(0, 0), 250, Luxor.julia_green, Luxor.julia_red)
+    red_green = blend(Point(0, 0), 5, Point(0, 0), 250, Luxor.julia_red, Luxor.julia_green)
+    green_purple = blend(Point(0, 0), 5, Point(0, 0), 250, Luxor.julia_green, Luxor.julia_purple)
+    # First point
+    setblend(green_red)
+    circle(point1, p_size, action=:fill)
+    # Second point
+    setblend(red_green)    
+    circle(point2, p_size, action=:fill)
+    # Third point
+    setblend(green_purple)
+    circle(point3, p_size, action=:fill)
 
     # Draw first lines
     setline(20)
