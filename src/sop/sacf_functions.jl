d3cfa656547d6033a90c0d7942e9f347062cdd6e
Philipp Adaemmer <adaemmerp@uni-greifswald.de>
2024-12-21 11:54:29 +0100

    improve sacf performance

--- src/sop/sacf_functions.jl
+++ src/sop/sacf_functions.jl
@@ -46,32 +46,53 @@
 - `d2::Int`: The number of column delays to consider for the SACF. The default is one.    
 """
 # Spatial ACF 
-function sacf(data, cdata, covs, d1::Int, d2::Int)
+function sacf(X_centered, d1::Int, d2::Int)
 
-  cdata .= data .- mean(data)
-  M = size(cdata, 1)
-  N = size(cdata, 2)
+  M = size(X_centered, 1)
+  N = size(X_centered, 2)
 
-  # Loop to compute all sums of relevant products
-  for k in 0:d2
-    for l in 0:d1
-      for j in 1:(N-l)
-        for i in 1:(M-k)
-          covs[l+1, k+1] += cdata[i+k, j+l] * cdata[i, j]
-        end
-      end
-    end
-  end
-
-  # Normalize by the number of elements
-  covs .= covs ./ (M * N)
-
-  # Compute the SACF value
-  sacf_val = covs[1+d1, 1+d2] / covs[1, 1]
+  # Lag 0x0
+  @views cov_00 = dot(X_centered[1:(M), 1:(N)], X_centered[1:M, 1:N]) / (M * N)
+  # Lag d1xd2
+  @views cov_d1d2 = dot(X_centered[1:(M-d1), 1:(N-d2)], X_centered[(1+d1):M, (1+d2):N]) / (M * N)
 
   # Return the SACF value
-  return sacf_val
+  if allequal(X_centered)
+    return 0.0
+  else
+    return cov_d1d2 / cov_00
+  end
 end
+
+
+# # Spatial ACF 
+# function sacf(data, cdata, covs, d1::Int, d2::Int)
+
+#   cdata .= data .- mean(data)
+#   M = size(cdata, 1)
+#   N = size(cdata, 2)
+
+#   # Loop to compute all sums of relevant products
+#   for k in (0, d2) # 0:d2 -> we only need 0-0 and one particular combination
+#     for l in (0, d1) # 0:d1 -> we only need 0-0 and one particular combination
+#       # for (k, l) in zip((0, d2), (0, d1)) 
+#       for j in 1:(N-l)
+#         for i in 1:(M-k)
+#           covs[l+1, k+1] += cdata[i+k, j+l] * cdata[i, j]
+#         end
+#       end
+#     end
+#   end
+
+#   # Normalize by the number of elements
+#   covs .= covs ./ (M * N)
+
+#   # Compute the SACF value
+#   sacf_val = covs[1+d1, 1+d2] / covs[1, 1]
+
+#   # Return the SACF value
+#   return sacf_val
+# end
 
 # Compute SACF for one picture and for integer delays
 function stat_sacf(data::Union{SubArray,Matrix{T}}, d1::Int, d2::Int) where {T<:Real}
@@ -147,7 +168,7 @@ function stat_sacf(lam, data::Array{T,3}, d1_vec::Vector{Int}, d2_vec::Vector{In
   for i in axes(data, 3)
     for (i, (d1, d2)) in enumerate(d1_d2_combinations)
       rho_hat_all[i] = (1 - lam) * rho_hat_all[i] + lam * sacf(view(data, :, :, i), cdata, covs, d1, d2)
-      bp_stat += 2 * rho_hat_all[i]^2      
+      bp_stat += 2 * rho_hat_all[i]^2
     end
     bp_stats[i] = bp_stat
   end
@@ -295,7 +316,7 @@ function rl_sacf(m::Int, n::Int, d1::Int, d2::Int, lam, cl,
 
       # Compute ρ(d1,d2)-EWMA
       rho_hat = (1 - lam) * rho_hat + lam * sacf(data, cdata, covs, d1, d2)
- 
+
     end
 
     rls[r] = rl
@@ -435,7 +456,7 @@ function arl_sacf(lam, cl, sp_dgp::ICSP, d1_vec::Vector{Int}, d2_vec::Vector{Int
   if nprocs() == 1 # Threading
 
     # Make chunks for separate tasks (based on number of threads)        
-    chunks = Iterators.partition(1:reps, div(reps, Threads.nthreads())) 
+    chunks = Iterators.partition(1:reps, div(reps, Threads.nthreads()))
 
     # Run tasks: "Threads.@spawn" for threading, "pmap()" for multiprocessing
     par_results = map(chunks) do i
@@ -445,7 +466,7 @@ function arl_sacf(lam, cl, sp_dgp::ICSP, d1_vec::Vector{Int}, d2_vec::Vector{Int
   elseif nprocs() > 1 # Multi Processing
 
     # Make chunks for separate tasks (based on number of workers)    
-    chunks = Iterators.partition(1:reps, div(reps, nworkers())) 
+    chunks = Iterators.partition(1:reps, div(reps, nworkers()))
 
     par_results = pmap(chunks) do i
       rl_sacf(lam, cl, sp_dgp, d1_vec, d2_vec, i, dist)
@@ -473,7 +494,7 @@ function rl_sacf(lam, cl, sp_dgp::ICSP, d1_vec::Vector{Int}, d2_vec::Vector{Int}
   # Compute all d1-d2 combinations
   d1_d2_combinations = Iterators.product(d1_vec, d2_vec)
   rho_hat_all = zeros(length(d1_d2_combinations))
-  
+
   for r in axes(p_reps, 1)
 
     # rho_hat = 0.0
@@ -515,17 +536,17 @@ function arl_sacf(lam, cl, spatial_dgp::SpatialDGP, d1_vec::Vector{Int}, d2_vec:
   if nprocs() == 1 # Threading
 
     # Make chunks for separate tasks (based on number of threads)        
-    chunks = Iterators.partition(1:reps, div(reps, Threads.nthreads())) 
+    chunks = Iterators.partition(1:reps, div(reps, Threads.nthreads()))
 
     # Run tasks: "Threads.@spawn" for threading, "pmap()" for multiprocessing
     par_results = map(chunks) do i
-      Threads.@spawn rl_sacf(lam, cl, d1_vec, d2_vec, i, spatial_dgp, dist_error, dist_ao)   
+      Threads.@spawn rl_sacf(lam, cl, d1_vec, d2_vec, i, spatial_dgp, dist_error, dist_ao)
     end
 
   elseif nprocs() > 1 # Multi Processing
 
     # Make chunks for separate tasks (based on number of workers)    
-    chunks = Iterators.partition(1:reps, div(reps, nworkers())) 
+    chunks = Iterators.partition(1:reps, div(reps, nworkers()))
 
     par_results = pmap(chunks) do i
       rl_sacf(lam, cl, d1_vec, d2_vec, i, spatial_dgp, dist_error, dist_ao)
