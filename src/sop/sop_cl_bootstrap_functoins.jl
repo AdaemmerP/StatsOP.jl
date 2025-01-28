728a6dee838dd36a8d1a5d13e914d10c969544d5
Philipp Adaemmer <adaemmerp@uni-greifswald.de>
2025-01-28 10:47:18 +0100

    rename cl functions and put them into separate files

---
+++ src/sop/sop_cl_bootstrap_functoins.jl
@@ -0,0 +1,50 @@
+"""
+    cl_sop_bootstrap(
+   data::Array{T,3}, lam, L0, cl_init, d1, d2, reps=10_000;
+  chart_choice=3, jmin=3, jmax=7, verbose=false
+)
+
+Compute the SOP control limit for the EWMA-chart based on bootstraping. The function returns the control limit for a given average run.
+
+The input parameters are:
+
+- `data::Array{T,3}`: A 3D-array containing the data for the EWMA-chart.
+- `lam::Real`: The smoothing parameter for the EWMA-chart.
+- `L0::Real`: The average run length for the EWMA-chart.
+- `cl_init::Real`: The initial value for the control limit.
+- `d1::Int`: The first dimension of the data array.
+- `d2::Int`: The second dimension of the data array.
+- `reps::Int=10_000`: The number of bootstrap replications.
+- `chart_choice::Int`: The choice of the chart for the EWMA-chart. The default is 3.
+- `jmin`: The minimum number of values to change after the decimal point in the control limit.
+- `jmax`: The maximum number of values to change after the decimal point in the control limit.
+- `verbose::Bool`: A boolean to indicate whether to print the control limit and ARL for each iteration.
+"""
+function cl_sop_bootstrap(
+    data::Array{T,3}, lam, L0, cl_init, d1, d2, reps=10_000;
+    chart_choice=3, jmin=3, jmax=7, verbose=false
+) where {T<:Real}
+
+    p_array = compute_p_array(data, d1, d2; chart_choice=chart_choice)
+
+    L1 = 0.0
+    for j in jmin:jmax
+        for dh in 1:80
+            cl_init = cl_init + (-1)^j * dh / 10^j
+            L1 = arl_sop_bootstrap(
+                p_array, lam, cl_init, reps, chart_choice=chart_choice
+            )[1]
+            if verbose
+                println("cl = ", cl_init, "\t", "ARL = ", L1)
+            end
+            if (j % 2 == 1 && L1 < L0) || (j % 2 == 0 && L1 > L0)
+                break
+            end
+        end
+        cl_init = cl_init
+    end
+    if L1 < L0
+        cl_init = cl_init + 1 / 10^jmax
+    end
+    return cl_init
+end
---
+++ src/sop/sop_cl_ic_function.jl
@@ -0,0 +1,47 @@
+"""
+    cl_sop_ic(
+  sop_dgp::ICSP, lam, L0, cl_init, d1::Int, d2::Int, reps=10_000;
+  chart_choice=3, jmin=4, jmax=6, verbose=false
+)
+
+Compute the control limit for a given in-control process. The input parameters are:
+  
+- `sop_dgp`: The in-control spatial process (ICSP) to use for the control limit.
+- `lam::Float64`:  A scalar value for lambda for the EWMA chart.
+- `L0::Float64`: The desired average run length.
+- `cl_init::Float64`: The initial value for the control limit.
+- `d1::Int`: The first (row) delay for the spatial process.
+- `d2::Int`: The second (column) delay for the spatial process.
+- `reps::Int`: The number of replications to compute the ARL.
+- `chart_choice::Int`: The chart choice for the SOP chart.
+- `jmin`: The minimum number of values to change after the decimal point in the control limit.
+- `jmax`: The maximum number of values to change after the decimal point in the control limit.
+- `verbose::Bool`: A boolean to indicate whether to print the control limit and ARL for each iteration.
+"""
+function cl_sop_ic(
+    sop_dgp::ICSP, lam, L0, cl_init, d1::Int, d2::Int, reps=10_000;
+    chart_choice=3, jmin=4, jmax=6, verbose=false
+)
+
+    L1 = 0.0
+
+    for j in jmin:jmax
+        for dh in 1:80
+            cl_init = cl_init + (-1)^j * dh / 10^j
+            L1 = arl_sop_ic(sop_dgp, lam, cl_init, d1, d2, reps; chart_choice)[1]
+            if verbose
+                println("cl = ", cl_init, "\t", "ARL = ", L1)
+            end
+            if (j % 2 == 1 && L1 < L0) || (j % 2 == 0 && L1 > L0)
+                break
+            end
+        end
+        cl_init = cl_init
+    end
+
+    if L1 < L0
+        cl_init = cl_init + 1 / 10^jmax
+    end
+
+    return cl_init
+end
