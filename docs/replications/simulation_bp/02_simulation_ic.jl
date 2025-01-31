0b7bba7928328a762daa7b308331862a573418cf
Philipp Adaemmer <adaemmerp@uni-greifswald.de>
2025-01-31 09:38:01 +0100

    update ic bp simulation

--- docs/replications/simulation_bp/02_simulation_ic.jl
+++ docs/replications/simulation_bp/02_simulation_ic.jl
@@ -17,20 +17,12 @@ addprocs(5)
 BLAS.set_num_threads(1)
 
 
-#--- Compute critical limits for SACF
-# Build Matrix to store all critical limits
-cl_sacf_mat = zeros(length(MN_vec), length(d1d2_vec))
-cl_sacf_bp_mat = zeros(length(MN_vec), 1)
-
-cl_sop_mat = similar(cl_sacf_mat)
-cl_sop_mat_bp = similar(cl_sacf_bp_mat)
-
 # Vector and delay combinations
-reps = 10^6
+reps = 10^5
 lam = 0.1
 MN_vec = [(11, 11), (16, 16), (26, 26), (41, 26)]
 d1d2_vec = [(1, 1), (2, 2), (3, 3)]
-w = 3
+w_max = 3
 
 # Load critical values
 cl_sacf_mat = load_object("cl_sacf_delays.jld2")
@@ -38,13 +30,13 @@ cl_sop_mat = load_object("cl_sop_delays.jld2")
 cl_sacf_bp_mat = load_object("cl_sacf_bp.jld2")
 cl_sop_bp_mat = load_object("cl_sop_bp.jld2")
 
-
 # Pre-allocate matrices
-dist = [TDist(2), Exponential(1)]
-# SACF
+dist = [Normal(0, 1), TDist(2), Exponential(1)]
+
+# SACF 
 arl_ic_sacf_mat = zeros(length(MN_vec), length(d1d2_vec), length(dist))
 sd_ic_sacf_mat = similar(arl_ic_sacf_mat)
-arl_ic_sacf_bp_mat = zeros(length(MN_vec), w, length(dist))
+arl_ic_sacf_bp_mat = zeros(length(MN_vec), w_max, length(dist))
 sd_ic_sacf_bp_mat = similar(arl_ic_sacf_bp_mat)
 
 # SOP
@@ -79,10 +71,10 @@ for (k, dist) in enumerate(dist)
 end
 
 # --- Save matrices to JLD2 file
-jldsave("arl_sac_delays.jld2"; arl_sac_mat)
-jldsave("sd_sac_delays.jld2"; sd_sac_mat)
-jldsave("arl_sop_delays.jld2"; arl_ic_sop_mat)
-jldsave("sd_sop_delays.jld2"; sd_ic_sop_mat)
+jldsave("arl_ic_sacf_delays.jld2"; arl_ic_sacf_mat)
+jldsave("sd_ic_sacf_delays.jld2"; sd_ic_sacf_mat)
+jldsave("arl_ic_sop_delays.jld2"; arl_ic_sop_mat)
+jldsave("sd_ic_sop_delays.jld2"; sd_ic_sop_mat)
 
 # Loop for BP statistics
 for (k, dist) in enumerate(dist)
@@ -91,17 +83,23 @@ for (k, dist) in enumerate(dist)
     N = MN[2]
     sp_dgp = ICSP(M, N, dist)
 
-    for w in 1:w
+    for w in 1:w_max
       # Compute SACF-BP for one d1-d2 pair
-      sacf_bp_arl = arl_sacf_bp_ic(sp_dgp, lam, cl_sacf_bp_mat[i], w, reps)
-      arl_ic_sacf_bp_mat[i, k] = sacf_bp_arl[1]
-      sd_ic_sacf_bp_mat[i, k] = sacf_bp_arl[2]
+      sacf_bp_arl = arl_sacf_bp_ic(sp_dgp, lam, cl_sacf_bp_mat[i, w], w, reps)
+      arl_ic_sacf_bp_mat[i, w, k] = sacf_bp_arl[1]
+      sd_ic_sacf_bp_mat[i, w, k] = sacf_bp_arl[2]
 
       # Compute SOP-BP for one d1-d2 pair
-      sop_bp_arl = arl_sop_bp_ic(sp_dgp, lam, cl_sop_bp_mat[i], w, reps; chart_choice=3)
-      arl_ic_sop_bp_mat[i, k] = sop_bp_arl[1]
-      sd_ic_sop_bp_mat[i, k] = sop_bp_arl[2]
+      sop_bp_arl = arl_sop_bp_ic(sp_dgp, lam, cl_sop_bp_mat[i, w], w, reps; chart_choice=3)
+      arl_ic_sop_bp_mat[i, w, k] = sop_bp_arl[1]
+      sd_ic_sop_bp_mat[i, w, k] = sop_bp_arl[2]
       println("Progress -> k: $k, i: $i, w: $w")     
     end
   end
 end
+
+# Safe 
+jldsave("arl_ic_sacf_bp.jld2"; arl_ic_sacf_bp_mat)
+jldsave("sd_ic_sacf_bp.jld2"; sd_ic_sacf_bp_mat)
+jldsave("arl_ic_sop_bp.jld2"; arl_ic_sop_bp_mat)
+jldsave("sd_ic_sop_bp.jld2"; sd_ic_sop_bp_mat)
