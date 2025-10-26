79b31608deadfe5226f7e14e6b449710c19526db
Philipp Adaemmer <adaemmerp@uni-greifswald.de>
2025-10-26 19:12:42 +0100

    update discrete processes

--- src/op/op_dgp_functions.jl
+++ src/op/op_dgp_functions.jl
@@ -215,6 +215,178 @@ function update_dgp_op!(dgp::QAR1, x_long, eps_long, dist_error, d::Int)
     return @views x_long[1:d:end]
 end
 
+
+# -------------------------------------------------#
+# ---------------  INAR(1) methods  ---------------#
+# -------------------------------------------------#
+
+# Method to initialize INAR(1) when d is Int 
+function init_dgp_op!(dgp::INAR1, x_long, dist_error::Poisson, d::Int)
+    x_long[1] = rand(Poisson(dist_error.λ / (1 - dgp.α)))
+    for i in 2:lastindex(x_long)
+        x_long[i] = rand(Binomial(x_long[i-1], dgp.α)) + rand(dist_error)
+    end
+    # add noise
+    for i in axes(x_long, 1)
+        x_long[i] += rand()
+    end
+    return @views x_long[1:d:end]
+end
+
+# Method to update INAR(1) when d is Int 
+function update_dgp_op!(dgp::INAR1, x_long, dist_error::Poisson, d::Int)
+    for i in 1:(lastindex(x_long)-1)
+        x_long[i] = x_long[i+1]
+    end
+    x = floor(x_long[end-1])
+    x_long[end] = rand(Binomial(x, dgp.α)) + rand(dist_error) + rand()
+
+    return @views x_long[1:d:end]
+end
+
+# Method to initialize INAR(1) when d is Vector{Int}
+function init_dgp_op!(dgp::INAR1, x_long, dist_error::Poisson, d::Vector{Int})
+    x_long[1] = rand(Poisson(dist_error.λ / (1 - dgp.α)))
+    for i in 2:lastindex(x_long)
+        x_long[i] = rand(Binomial(x_long[i-1], dgp.α)) + rand(dist_error)
+    end
+    # add noise
+    for i in axes(x_long, 1)
+        x_long[i] += rand()
+    end
+    return @views x_long[d]
+end
+
+# Method to update INAR(1) when d is Vector{Int}
+function update_dgp_op!(dgp::INAR1, x_long, dist_error::Poisson, d::Vector{Int})
+    for i in 1:(lastindex(x_long)-1)
+        x_long[i] = x_long[i+1]
+    end
+    x = floor(x_long[end-1])
+    x_long[end] = rand(Binomial(x, dgp.α)) + rand(dist_error) + rand()
+    return @views x_long[d]
+end
+
+# -------------------------------------------------#
+# ---------------  BAR(1) methods  ----------------#
+# -------------------------------------------------#
+
+# Method to initialize BAR(1) when d is Int 
+function init_dgp_op!(dgp::BAR1, x_long, dist_error::Nothing, d::Int)
+    x_long[1] = rand(Binomial(dgp.n, dgp.parpi))
+    for i in 2:lastindex(x_long)
+        x = x_long[i-1]
+        x_long[i] = rand(Binomial(x, dgp.α)) + rand(Binomial(dgp.n - x, dgp.β))
+    end
+    # add noise
+    for i in axes(x_long, 1)
+        x_long[i] += rand()
+    end
+    return @views x_long[1:d:end]
+end
+
+# Method to update BAR(1) when d is Int 
+function update_dgp_op!(dgp::BAR1, x_long, dist_error::Nothing, d::Int)
+    for i in 1:(lastindex(x_long)-1)
+        x_long[i] = x_long[i+1]
+    end
+    x = floor(x_long[end-1])
+    x_long[end] = rand(Binomial(x, dgp.α)) + rand(Binomial(dgp.n - x, dgp.β)) + rand()
+    return @views x_long[1:d:end]
+end
+
+# Method to initialize BAR(1) when d is Vector{Int} 
+function init_dgp_op!(dgp::BAR1, x_long, dist_error::Nothing, d::Vector{Int})
+    x_long[1] = rand(Binomial(dgp.n, dgp.parpi))
+    for i in 2:lastindex(x_long)
+        x = x_long[i-1]
+        x_long[i] = rand(Binomial(x, dgp.α)) + rand(Binomial(dgp.n - x, dgp.β))
+    end
+    # add noise
+    for i in axes(x_long, 1)
+        x_long[i] += rand()
+    end
+    return @views x_long[d]
+end
+
+# Method to update BAR(1) when d is Vector{Int}
+function update_dgp_op!(dgp::BAR1, x_long, dist_error::Nothing, d::Vector{Int})
+    for i in 1:(lastindex(x_long)-1)
+        x_long[i] = x_long[i+1]
+    end
+    x = floor(x_long[end-1])
+    x_long[end] = rand(Binomial(x, dgp.α)) + rand(Binomial(dgp.n - x, dgp.β)) + rand()
+    return @views x_long[d]
+end
+
+# -------------------------------------------------#
+# ---------------  DAR(1) methods  ----------------#
+# -------------------------------------------------#
+
+# Method to initialize DAR(1) when d is Int 
+function init_dgp_op!(dgp::DAR1, x_long, dist_error, d::Int)
+    x_long[1] = rand(dist_error)
+    for i in 2:lastindex(x_long)
+        if rand(Binomial(1, dgp.α)) == 0
+            x_long[i] = rand(dist_error)
+        else
+            x_long[i] = x_long[i-1]
+        end
+    end
+    # add noise
+    for i in axes(x_long, 1)
+        x_long[i] += rand()
+    end
+    return @views x_long[1:d:end]
+end
+
+# Method to update DAR(1) when d is Int
+function update_dgp_op!(dgp::DAR1, x_long, dist_error, d::Int)
+    for i in 1:(lastindex(x_long)-1)
+        x_long[i] = x_long[i+1]
+    end
+    if rand(Binomial(1, dgp.α)) == 0
+        x_long[end] = rand(dist_error) + rand()
+    else
+        x_long[end] = floor(Int, x_long[end-1]) + rand()
+    end
+    return @views x_long[1:d:end]
+end
+
+# Method to initialize DAR(1) when d is Vector{Int}
+function init_dgp_op!(dgp::DAR1, x_long, dist_error, d::Vector{Int})
+    x_long[1] = rand(dist_error)
+    for i in 2:lastindex(x_long)
+        if rand(Binomial(1, dgp.α)) == 0
+            x_long[i] = rand(dist_error)
+        else
+            x_long[i] = x_long[i-1]
+        end
+    end
+    # add noise
+    for i in axes(x_long, 1)
+        x_long[i] += rand()
+    end
+    return @views x_long[d]
+end
+
+# Method to update DAR(1) when d is Vector{Int}
+function update_dgp_op!(dgp::DAR1, x_long, dist_error, d::Vector{Int})
+    for i in 1:(lastindex(x_long)-1)
+        x_long[i] = x_long[i+1]
+    end
+    if rand(Binomial(1, dgp.α)) == 0
+        x_long[end] = rand(dist_error) + rand()
+    else
+        x_long[end] = x_long[end-1]
+    end
+    return @views x_long[d]
+end
+
+
+
+
+
 # -------------------------------------------------#
 # -------  Methods for d as Vector{Int} -----------#
 # -------------------------------------------------#
